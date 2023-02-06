c8220d29298032e147efa49c87aa395bf82119a1
Panagiotis Karamolegkos <p.karamolegos@yahoo.gr>
2023-02-06 04:13:23 +0200

    Update MovieAPI.java
    Update MovieAPI.java

--- src/main/java/my/app/MovieAPI.java
+++ src/main/java/my/app/MovieAPI.java
@@ -5,7 +5,7 @@ import services.MovieDBService;
 public class MovieAPI {
 	
 	public static MovieDBService getMovieDBService() {
-		return new MovieDBService("https://api.themoviedb.org/", "3598a6c5675de9969e2694550212bf88");
+		return new MovieDBService("https://api.themoviedb.org/", "your-key-goes-here");
 	}
 
 }
